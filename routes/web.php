3407155d42c23598f3eafcc016a8e2eca540fd1e
Muhammad Khaidir <khaedir3018@gmail.com>
2025-08-14 11:21:52 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Backend\RoleController;
 
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
