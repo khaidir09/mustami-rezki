a867eb27fe7d96f6ce6f23617092a85549e6cefb
Muhammad Khaidir <khaedir3018@gmail.com>
2025-11-29 22:31:03 +0800

    Update sidebar.blade.php

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -29,6 +29,8 @@
                 </li>
 
                 @if (Auth::user()->hasRole('Super Admin') || Auth::user()->hasRole('Admin'))
+                    <li class="menu-title">Master Data</li>
+                    
                     <li>
                         <a href="{{ route('all.category') }}" class="tp-link">
                             <i data-feather="package"></i>
@@ -42,29 +44,47 @@
                             Produk
                         </a>
                     </li>
-                @endif
 
-                @if (Auth::user()->hasRole('Super Admin'))
-                    <li>
-                        <a href="{{ route('all.supplier') }}" class="tp-link">
-                            <i data-feather="users"></i>
-                            Supplier
-                        </a>
-                    </li>
-                @endif
+                    @if (Auth::user()->hasRole('Super Admin'))
+                        <li>
+                            <a href="{{ route('all.supplier') }}" class="tp-link">
+                                <i data-feather="users"></i>
+                                Supplier
+                            </a>
+                        </li>
+                    @endif
 
-                @if (Auth::user()->hasRole('Super Admin') || Auth::user()->hasRole('Admin'))
                     <li>
                         <a href="{{ route('all.customer') }}" class="tp-link">
                             <i data-feather="users"></i>
                             Pelanggan
                         </a>
                     </li>
+                @endif
 
+                <li class="menu-title">Transaksi</li>
+
+                @if (Auth::user()->hasRole('Super Admin'))
+                    <li>
+                        <a href="{{ route('all.purchase') }}" class="tp-link">
+                            <i data-feather="aperture"></i>
+                            Pembelian Barang
+                        </a>
+                    </li>
+                @endif
+
+                @if (Auth::user()->hasRole('Super Admin') || Auth::user()->hasRole('Admin'))
                     <li>
                         <a href="{{ route('all.sale') }}" class="tp-link">
                             <i data-feather="shopping-bag"></i>
                             Penjualan
+                        </a>
+                    </li>
+                    
+                     <li>
+                        <a href="{{ route('all.production') }}" class="tp-link">
+                            <i data-feather="pen-tool"></i>
+                            Produksi
                         </a>
                     </li>
 
@@ -83,41 +103,48 @@
                     </a>
                 </li>
 
-                @if (Auth::user()->hasRole('Super Admin'))
-                    <li>
-                        <a href="{{ route('profit.distribution.report') }}" class="tp-link">
-                            <i data-feather="dollar-sign"></i>
-                            Laporan Profit
-                        </a>
-                    </li>
-                @endif
-
                 @if (Auth::user()->hasRole('Super Admin') || Auth::user()->hasRole('Admin'))
+                    <li class="menu-title">Keuangan</li>
+
                     <li>
                         <a href="{{ route('all.expense') }}" class="tp-link">
                             <i data-feather="dollar-sign"></i>
                             Pengeluaran
                         </a>
                     </li>
+                    
+                    @if (Auth::user()->hasRole('Super Admin'))
+                         <li>
+                            <a href="{{ route('all.acceptance') }}" class="tp-link">
+                                <i data-feather="dollar-sign"></i>
+                                Penerimaan Eksternal
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('daily.financial.index') }}" class="tp-link">
+                                <i data-feather="dollar-sign"></i>
+                                Arus Kas Harian
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <a href="{{ route('financial.index') }}" class="tp-link">
+                                <i data-feather="dollar-sign"></i>
+                                Arus Kas Bulanan
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <a href="{{ route('profit.distribution.report') }}" class="tp-link">
+                                <i data-feather="dollar-sign"></i>
+                                Laporan Profit
+                            </a>
+                        </li>
+                    @endif
                 @endif
 
-                @if (Auth::user()->hasRole('Super Admin'))
-                    <li>
-                    <a href="{{ route('all.purchase') }}" class="tp-link">
-                        <i data-feather="aperture"></i>
-                            Pembelian Barang
-                        </a>
-                    </li>
-                @endif
-
-                @if (Auth::user()->hasRole('Super Admin') || Auth::user()->hasRole('Admin'))
-                <li>
-                    <a href="{{ route('all.production') }}" class="tp-link">
-                        <i data-feather="pen-tool"></i>
-                        Produksi
-                    </a>
-                </li>
-                @endif
+                <li class="menu-title">Kepegawaian</li>
 
                 <li>
                     <a href="{{ route('attendances.index') }}" class="tp-link">
@@ -134,81 +161,7 @@
                 </li>
 
                 @if (Auth::user()->hasRole('Super Admin'))
-                    <li>
-                        <a href="{{ route('all.acceptance') }}" class="tp-link">
-                            <i data-feather="briefcase"></i>
-                            Penerimaan Eksternal
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('daily.financial.index') }}" class="tp-link">
-                            <i data-feather="briefcase"></i>
-                            Arus Kas Harian
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('financial.index') }}" class="tp-link">
-                            <i data-feather="briefcase"></i>
-                            Arus Kas Bulanan
-                        </a>
-                    </li>
-                @endif
-
-                {{-- 
-                <li>
-                    <a href="#Due" data-bs-toggle="collapse">
-                    <i data-feather="briefcase"></i>
-                        <span> Due Setup </span>
-                        <span class="menu-arrow"></span>
-                    </a>
-                    <div class="collapse" id="Due">
-                        <ul class="nav-second-level">
-                            <li>
-                                <a href="{{ route('due.sale') }}" class="tp-link">Sales Due</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('due.sale.return') }}" class="tp-link">Sales Return Due</a>
-                            </li>
-                            
-                        </ul>
-                    </div>
-                </li>
-
-                <li>
-                    <a href="#Transfers" data-bs-toggle="collapse">
-                        <i data-feather="table"></i>
-                        <span> Transfers Setup </span>
-                        <span class="menu-arrow"></span>
-                    </a>
-                    <div class="collapse" id="Transfers">
-                        <ul class="nav-second-level">
-                            <li>
-                                <a href="{{ route('all.transfer') }}" class="tp-link">Transfers </a>
-                            </li>
-                            
-                            
-                        </ul>
-                    </div>
-                </li>
-
-                <li>
-                    <a href="#Report" data-bs-toggle="collapse">
-                    <i data-feather="pie-chart"></i>
-                        <span> Report Setup </span>
-                        <span class="menu-arrow"></span>
-                    </a>
-                    <div class="collapse" id="Report">
-                        <ul class="nav-second-level">
-                            <li>
-                                <a href="{{ route('all.report') }}" class="tp-link">All Reports </a>
-                            </li>
-                            
-                            
-                        </ul>
-                    </div>
-                </li> --}}
-
-                @if (Auth::user()->hasRole('Super Admin'))
+                    <li class="menu-title">Sistem</li>
                     <li>
                         <a href="{{ route('all.admin') }}" class="tp-link">
                             <i data-feather="map"></i>
@@ -216,6 +169,7 @@
                         </a>
                     </li>
                 @endif
+
             </ul>
 
         </div>
